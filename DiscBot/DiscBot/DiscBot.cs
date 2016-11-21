803f0e001563ca5b47998f5b2a694aaaf2dd69af
Sean Thomas <sean.daxxtrias@gmail.com>
2016-11-20 18:56:24 -0600

    Added a global scope variable as a container and re-utilize it with various calls, rather then constantly re-initializing a new variable. Added the initial constructor for this variable to a on connected event, and did some code cleaning

--- DiscBot/DiscBot/DiscBot.cs
+++ DiscBot/DiscBot/DiscBot.cs
@@ -14,6 +14,7 @@ namespace DiscBot
         static void Main() => new DiscBot().Start();
 
         public static DiscordClient Client { get; private set; }
+        public static Channel logChannel { get; private set; }
         private static TimeSpan UpTime { get; set; }
         private string prettyCurrentTime => $"【{DateTime.Now:HH:mm:ss}】";
         private string curruntRunningTime => $" [{UpTime:g}] ";
@@ -67,20 +68,13 @@ namespace DiscBot
             Client.UserJoined += UsrJoined;
             Client.UserBanned += UsrBanned;
             Client.UserUpdated += UsrUpdtd;
-
-
-            // echo bot (initial test code, now zombie code)
-            //Client.MessageReceived += async (s, e) =>
-            //{
-            //    if (!e.Message.IsAuthor)
-            //        await e.Channel.SendMessage(e.Message.Text);
-            //};
+            Client.ServerAvailable += SrvUpdtd;
 
             Client.ExecuteAndWait(async () =>
             {
                 await Client.Connect("MjQ5MDU0MDk5MDM3NzQ5MjUw.CxAtZQ.0PAe-hQifY4yBuz3dPudWEgfk6s", TokenType.Bot);
                 Console.Title = "DiscBot";
-                Console.WriteLine("DiscBot Running and logged into discord!");
+                Console.WriteLine("DiscBot is now running and has successfully connected");
             });
 
         }
@@ -89,14 +83,15 @@ namespace DiscBot
         {
             //todo permissions / user limit changes are also caught, but currently passed over
             string str = $"`{prettyCurrentTime}`";
-            var logchannel = e.Server.FindChannels("logs").FirstOrDefault();
+            if (logChannel == null)
+                logChannel = e.Server.FindChannels("logs").FirstOrDefault();
 
             if (e.Before.Name != e.After.Name)  /* (*{e.After.Id}*) */
-                await logchannel.SendMessage($@"`{prettyCurrentTime}` **Channel Name Changed** `#{e.Before.Name}`
+                await logChannel.SendMessage($@"`{prettyCurrentTime}` **Channel Name Changed** `#{e.Before.Name}`
                                             `New:` {e.After.Name}").ConfigureAwait(false);
 
             else if (e.Before.Topic != e.After.Topic) /* (*{e.After.Id}*) */
-                await logchannel.SendMessage($@"`{prettyCurrentTime}` **Channel Topic Changed** `#{e.After.Name}`
+                await logChannel.SendMessage($@"`{prettyCurrentTime}` **Channel Topic Changed** `#{e.After.Name}`
                                             `Old:` {e.Before.Topic} `New:` {e.After.Topic}").ConfigureAwait(false);
 
             //await logchannel.SendMessage(str + "**" + e.Before.Name + "** channel has been modified");
@@ -105,6 +100,17 @@ namespace DiscBot
         private async void ChannelDestroyed(object sender, ChannelEventArgs e)
         {
 
+        }
+        private async void SrvUpdtd(object sender, ServerEventArgs e)
+        {
+            // Triggers upon successfull connection
+
+            // Should probably have this update anytime this event triggers?
+            logChannel = e.Server.FindChannels("logs").FirstOrDefault();
+            if (logChannel != null)
+            {
+                Console.WriteLine("Server update event, log channel identified as: " + logChannel.Name);
+            }
         }
         private async void ChannelCreated(object sender, ChannelEventArgs e)
         {
@@ -116,22 +122,25 @@ namespace DiscBot
         }
         private async void UsrJoined(object sender, UserEventArgs e)
         {
-            var logchannel = e.Server.FindChannels("logs").FirstOrDefault();
-            await logchannel.SendMessage("`{ prettyCurrentTime}` " + e.User.Mention + " has joined the server");
+            if (logChannel == null)
+                logChannel = e.Server.FindChannels("logs").FirstOrDefault();
+            await logChannel.SendMessage("`{ prettyCurrentTime}` " + e.User.Mention + " has joined the server");
             Console.WriteLine("[" + e.Server.Name + "] " + e.User.Name + "#" + e.User.Discriminator + " has joined the server");
             // should we turn this into a greet bot too?
             //await e.Server.DefaultChannel.SendMessage("Please welcome " + e.User.Mention + " to the server!");
         }
         private async void UsrLeft(object sender, UserEventArgs e)
         {
-            var logchannel = e.Server.FindChannels("logs").FirstOrDefault();
-            await logchannel.SendMessage("`{ prettyCurrentTime}` " + e.User.Mention + " has Left the server");
+            if (logChannel == null)
+                logChannel = e.Server.FindChannels("logs").FirstOrDefault();
+            await logChannel.SendMessage("`{ prettyCurrentTime}` " + e.User.Mention + " has Left the server");
             Console.WriteLine("[" + e.Server.Name + "] " + e.User.Name + "#" + e.User.Discriminator + " has left the server");
         }
         private async void UsrBanned(object sender, UserEventArgs e)
         {
-            var logchannel = e.Server.FindChannels("logs").FirstOrDefault();
-            await logchannel.SendMessage(e.User.Mention + " has been banned from the server");
+            if (logChannel == null)
+                logChannel = e.Server.FindChannels("logs").FirstOrDefault();
+            await logChannel.SendMessage(e.User.Mention + " has been banned from the server");
             Console.WriteLine("[" + e.Server.Name + "] " + e.User.Name + "#" + e.User.Discriminator + " has been banned from the server");
         }
         private async void UsrUpdtd(object sender, UserUpdatedEventArgs e)
@@ -140,7 +149,8 @@ namespace DiscBot
             {
                 // permission changes, gone offline, changed channel
                 // also updates if users deafen/undeafen
-                var logchannel = e.Server.FindChannels("logs").FirstOrDefault();
+                if (logChannel == null)
+                    logChannel = e.Server.FindChannels("logs").FirstOrDefault();
                 string str = $"`{prettyCurrentTime}`";
                 var beforeVch = e.Before.VoiceChannel;
                 var afterVch = e.After.VoiceChannel;
@@ -148,16 +158,14 @@ namespace DiscBot
 
                 if ((beforeVch != null || afterVch != null) && (beforeVch != afterVch))
                 {
-                    //if (e.Before.VoiceChannel == null || e.After.VoiceChannel == null)
-                    //    return;
                     if (afterVch != null)
                     {
                         if (afterVch.Name == "Raid")
-                            str += $"{e.After.Name} has joined **{afterVch}** voice channel.";
+                            str += $"**{e.After.Name}** has joined **{afterVch}** voice channel.";
                         if (beforeVch != null)
                         {
                             if (beforeVch.Name == "Raid" && afterVch.Name != "Raid")
-                                str += $"{e.Before.Name} has left **{beforeVch}** voice channel.";
+                                str += $"**{e.Before.Name}** has left **{beforeVch}** voice channel.";
                         }
                         else
                         {
@@ -166,15 +174,6 @@ namespace DiscBot
                         }
                     }
 
-                    //if (beforeVch != null)
-                    //{
-                    //    if (beforeVch.Name == "Raid")
-                    //        str += $"{e.Before.Name} has left **{beforeVch}** voice channel.";
-                    //    // this 1 is probably redundant / never utilized
-                    //    //todo find a better way to do this sort of thing
-                    //    else
-                    //        return;
-                    //}
                     else
                         return;
                 }
@@ -203,30 +202,24 @@ namespace DiscBot
                         Console.WriteLine("SEQUENCE NOT EQUAL BUT NO DIFF ROLES - REPORT TO ADMIN");
                         return;
                     }
-
                 }
                 else
                 {
                     Console.WriteLine("Unhandled trigger in general comparison");
                     return;
                 }
-
-
                 if (str != $"`{prettyCurrentTime}`")
                 {
-                    await logchannel.SendMessage(str);
+                    await logChannel.SendMessage(str);
                     Console.WriteLine(str);
                 }
                 else
                 {
                     // This seems to occur when a user moves from a channel we're not filtering for
                     // Investigate why its not being picked up earlier (beside the fact my code is fugly)
-                    //await logchannel.SendMessage("");
+                    //await logChannel.SendMessage("");
                     Console.WriteLine("Final unhandled triggered, investigate if this triggers");
                 }
-
-                //await logchannel.SendMessage(e.Before.Mention + " has had their permissions modified");
-                //Console.WriteLine("[" + e.Server.Name + "] " + e.Before.Name + "#" + e.Before.Discriminator + " has had their permissions modified");
             }
             catch (Exception)
             {
